df3bca615d9b6d3a172b00813d32456a77ef450f
Vimansha Madushani <149356239+vimanshaMJ@users.noreply.github.com>
2024-10-07 19:19:07 +0530

    Add files via upload

---
+++ ac/post.php
@@ -0,0 +1,159 @@
+<?php
+	$pageId = "ac";
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+
+	<meta charset="UTF-8" />
+	<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0" />
+	<title>Department of Geography</title>
+
+	<?php require_once "../assets/header.php"; ?>
+	
+	<!-- begin body content -->
+
+	<div class="container">
+		<ol class="breadcrumb">
+		  <li><a href="../index.php">Home</a></li>
+		  <li class="active">Academic</li>
+		  <li class="active">Postgraduate Programme</li>
+		</ol>
+            <div class="page">
+            <h3>Postgraduate Programme</h3>
+                
+            <hr />
+
+            
+            <p>The Department offers courses leading to M.A. and M.Phil Degrees and provides opportunities to pursue their research interests in the Ph. D. Degree programs. Annual intake of postgraduate students is in the range of 60-70. 
+The students should have at least a second class honors pass in their first degree or higher qualifications to register for a M.A. or M. Phil. Program. Those who do not possess these qualifications should obtain the necessary qualifications by completing post graduate diploma courses and sit for the examination.</p>
+
+			<h4>Postgraduate Diploma</h4>
+
+            <p>Post Graduate Diploma students have to follow five courses. The courses; GGY 501, GGY 502 and GGY 505 are compulsory. In addition they must select two optional courses. The evaluation of each course consists of a final examination (carry 60% of the marks) and two assignments (carry 40% of the marks) written on individual selected, pertinent topics.</p>
+            <p>To pass, students have to earn at least 50% (30 marks) in the final examination and 50% (20 marks) in the assignments in each course. Lectures in the Diploma programme are conducted in English and the total number of teaching hours is 35.An additional 10 hours of discussion are allowed in Sinhala / Tamil.</p>
+            <!--<br />
+
+            <h4>Name of The Course and Instructions</h4>
+            <div class="table-responsive">
+                <table class="table table-hover">
+                    <thead>
+                        <tr>
+                            <th colspan="3">500 Level Course Instructor</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>501</td>
+                            <td>Theoretical Perspective</td>
+                            <td>- Prof. Daya Amarasekara</td>
+                        </tr>
+                        <tr>
+                            <td>502</td>
+                            <td>Theories of Society</td>
+                            <td>- Mr. W. M. S. N. Wijesinghe</td>
+                        </tr>
+                        <tr>
+                            <td>503</td>
+                            <td>Research Methods</td>
+                            <td>- Dr. M. G. Manurathna</td>
+                        </tr>
+                        <tr>
+                            <td>504</td>
+                            <td>Sociology of South Asia</td>
+                            <td>- Prof. H. M. D. R. Herath</td>
+                        </tr>
+                    </tbody>	
+                </table>
+            </div>
+            <div class="table-responsive">	
+                <table class="table table-hover">
+                    <thead>
+                        <tr>
+                            <th colspan="3">M.A / M.Phil Course Instructor</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>601</td>
+                            <td>Rural Sociology</td>
+                            <td>- Dr. M. G. Manurathna</td>
+                        </tr>
+                        <tr>
+                            <td>602</td>
+                            <td>Issues in Third World Development</td>
+                            <td>- Prof. S. A. Karunatissa</td>
+                        </tr>
+                        <tr>
+                            <td>603</td>
+                            <td>Women in South Asia</td>
+                            <td>- Dr. Mallika Pinnawala</td>
+                        </tr>
+                        <tr>
+                            <td>604</td>
+                            <td>Ethnicity in the Third World</td>
+                            <td>- Prof. Sisira Pinnawala</td>
+                        </tr>
+                        <tr>
+                            <td>605</td>
+                            <td>Sociology of Poverty</td>
+                            <td>- Prof. K. Tudor Silva</td>
+                        </tr>
+                        <tr>
+                            <td>606</td>
+                            <td>Applied Sociology</td>
+                            <td>- Prof. S. A. Karunatissa</td>
+                        </tr>
+                        <tr>
+                            <td>607</td>
+                            <td>Medical Anthropology</td>
+                            <td>- Dr. Abey Rathnayake</td>
+                        </tr>
+                    </tbody>	
+                </table>
+            </div>-->
+            <span>GGY 501</span> <span>Research Methodology</span> <br/>
+            <span>GGY 502</span> <span>Sri Lanka Geographical Themes</span> <br/>
+            <span>GGY 503</span> <span>Resource Management and Strategic Planning</span> <br/>
+            <span>GGY 504</span> <span>Development Geography</span> <br/>
+            <span>GGY 505</span> <span>Recent Trends in Geographic Thought</span> <br/>
+            
+            <!--<p>M.A. Preliminary Programme M.A. Preliminary Programme of the Department of Sociology is introduced to replace the existing Master of Arts Qualifying Examination. The Main objective of the preliminary Programme is to equip graduate students with a knowledge of sociological theory and methods and with a background knowledge of Society and culture of South Asia in order to prepare them adequately for the M.A. Programme. <strong>The Following three courses offered by the Department are compulsory.</strong></p>
+
+            <ul>
+                <li>SS 5.1 Theories of Society</li>
+                <li>SS 5.2 Methods in Social Research</li>
+                <li>SS 5.3 South Asian Society</li>
+            </ul>-->
+
+            <hr />
+
+            <h4>Master of Arts (MA)/Master of Philosophy (M.Phil)</h4>
+            
+            <p>MA/M.Phil students have to follow six courses. The course GGY 601 is compulsory for MA/M.Phil students. In addition they must select five optional courses from a range of diverse topics relevant to their interest. The evaluation of each course consists of a final examination (carry 60% of the marks) and two assignments (carry 40% of the marks) written on individual selected, pertinent topics. Students have to earn at least 30 marks in the final examination and 20marks in the assignments in each course to pass the examination. Lecture in the MA/M.Phil are conducted in English.</p>
+            
+            <span>GGY 601</span> <span>Research Techniques in Geography</span> <br/>
+            <span>GGY 602</span> <span>Gender in Development Planning</span> <br/>
+            <span>GGY 603</span> <span>Tropical Agro forestry Systems</span> <br/>
+            <span>GGY 604</span> <span>Geography and Nationalism</span> <br/>
+            <span>GGY 605</span> <span>Medical Geography</span> <br/>
+            <span>GGY 606</span> <span>GIS and Remote Sensing</span> <br/>
+            <span>GGY 607</span> <span>GIS and Society</span> <br/>
+            <span>GGY 608</span> <span>Spatial Strategies of Industrial Development</span> <br/>
+            <span>GGY 609</span> <span>Urban Development and Planning</span> <br/>
+            <span>GGY 610</span> <span>Landscape Ecology</span> <br/>
+            <span>GGY 611</span> <span>Climate Change and Human Response</span> <br/>
+            <span>GGY 612</span> <span>Environmental Management</span> <br/>
+            <span>GGY 613</span> <span>Human Resource Development Planning</span> <br/>
+            
+            
+            
+            </div>
+        </div>
+
+		<hr />
+
+	<?php require_once "../assets/footer.php" ?>
+</body>
+</html>
---
+++ ac/under.php
@@ -0,0 +1,83 @@
+<?php
+	$pageId = "ac";
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+
+	<meta charset="UTF-8" />
+	<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0" />
+	<title>Department of Archaeology</title>
+
+	<?php require_once "../assets/header.php"; ?>
+	
+	<!-- begin body content -->
+
+	<div class="container">
+		<ol class="breadcrumb">
+		  <li><a href="../index.php">Home</a></li>
+		  <li class="active">Programmes</li>
+		  <li class="active">Undergraduate Programme</li>
+		</ol>
+        <div class="page">
+            <h3>Undergraduate Programme</h3>
+            <hr />
+            <div class="cList">
+            <h3>Our Course Structure</h3>
+            <h4>First Year Courses</h4>
+           <!-- <span>ACL 101</span> <span>Introductory Archaeology I </span><br/>-->
+            <ul>
+           <li>ACL 1001 Introductory Archaeology I </li>
+		   <li>ACL 1002 Introductory Archaeology II </li>
+			</ul>
+           
+          <h4>Second Year Courses</h4>
+          
+<li>ACL 2001 Palaeo-Human Biology and Ethno-Archaeology</li>
+<li>ACL 2002 Socio-Economic History of Ancient India</li>
+<li>ACL 2003 Archaeological Theory and Methods</li>
+<li>ACL 2004 Cultural Resource Management</li>
+<li>ACL 2005 Conservation of Archaeological Material</li>
+<li>ACL 2006 Formation of Ancient Civilizations</li>
+<li>ACL 2007 The Socio-Economic History of Ancient Sri Lanka</li>
+<li>ACL 2008 Practical Archaeology</li>
+<li>ACL 2009 Heritage Museum and Management</li>
+
+          
+           
+          <h4>Third Year Courses</h4>
+           
+<li>ACL 3001 Crafts & Technology of Pre-Industrial South Asia</li>
+<li>ACL 3002 Epigraphic Studies of Ancient India</li>
+<li>ACL 3003 The Built Environment of Ancient India</li>
+<li>ACL 3004 Coinage Systems of Ancient India</li>
+<li>ACL 3005 Coinage Systems of Ancient Sri Lanka</li>
+<li>ACL 3006 The Built environment of Ancient Sri Lanka</li>
+<li>ACL 3007 Epigraphic Studies of Ancient Sri Lanka</li>
+<li>ACL 3008 Archaeological Data Retrieval Systems & Processes</li>
+<li>ACL 3020 Bio-Archaeology</li>
+
+            
+             
+          <h4>Fourth Year Courses</h4> 
+<li>ACL 4001 Environmental Archaeology</li>
+<li>ACL 4002 World Prehistory</li>
+<li>ACL 4003 The Bronze Age & Early Iron Age Archaeology of Ancient India</li>
+<li>ACL 4004 Ancient Cultural Ecology</li>
+<li>ACL 4005 Prehistory of Sri Lanka</li>
+<li>ACL 4006 Early Iron Age Archaeology of Sri Lanka the Formative Period</li>
+<li>ACL 4999 Dissertation</li>
+  
+          <br/>        
+           
+            
+                </div>
+                
+           </div>
+        </div>
+	</div>
+
+	<?php require_once "../assets/footer.php" ?>
+</body>
+</html>
